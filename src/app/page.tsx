d87e6a40bc5f3ff4786bd4bacbd27722fba0e728
ActuallyPanda56 <nietoc0595@gmail.com>
2024-11-11 16:58:05 -0500

    fix desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,19 +57,19 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center">
       <main className="flex flex-col gap-32">
-        <div className="flex flex-col md:flex-row justify-between gap-8">
-          <div className="flex flex-col w-full md:w-2/5 gap-8">
-            <h2 className="text-4xl md:text-5xl 2xl:text-6xl leading-[60px] md:leading-[65px] 2xl:leading-[85px] font-semibold">
+        <div className="flex justify-between">
+          <div className="flex flex-col w-2/5 gap-8">
+            <h2 className="text-6xl leading-[85px] font-semibold">
               Navigating the digital landscape for success
             </h2>
-            <p className="text-lg md:text-2xl tracking-wide">
+            <p className="text-2xl  tracking-wide">
               Our digital marketing agency helps businesses grow and succeed
               online through a range of services including SEO, PPC, social
               media marketing, and content creation.
             </p>
             <Link
               href="/request-a-quote"
-              className="py-3 px-6 md:py-5 md:px-8 text-white text-lg bg-secondary rounded-xl w-fit flex items-center justify-center hover:scale-105 transition-all"
+              className="py-5 px-8 text-white text-lg bg-secondary rounded-xl w-fit flex items-center justify-center hover:scale-105 transition-all"
             >
               Book a consultation
             </Link>
@@ -77,73 +77,69 @@ export default function Home() {
           <Image
             src="/assets/megaphone.svg"
             alt="Hero"
-            width={400}
-            height={400}
-            className="w-full md:w-[500px]"
+            width={500}
+            height={500}
           />
         </div>
-
-        <div className="flex flex-wrap justify-center gap-6 my-12">
+        <div className="flex justify-between">
           {logos.map(({ src, alt }) => (
             <Image
               src={src}
               alt={alt}
-              width={150}
-              height={150}
+              width={200}
+              height={200}
               key={src}
-              className="w-24 h-auto grayscale"
+              className="w-32 h-auto grayscale"
             />
           ))}
         </div>
-
         {sections.slice(0, 1).map(({ title, description, component }) => (
-          <div className="flex flex-col gap-24 px-4 md:px-0" key={title}>
-            <div className="flex flex-col md:flex-row gap-10 w-full md:w-2/3">
-              <h2 className="px-4 py-2 text-3xl md:text-5xl bg-primary rounded-lg font-semibold text-center md:text-left flex items-center justify-center">
+          <div className="flex flex-col gap-24" key={title}>
+            <div className="flex gap-10 w-2/3">
+              <h2 className="px-2 py-1 text-5xl bg-primary rounded-lg font-semibold flex items-center justify-center text-nowrap">
                 {title}
               </h2>
-              <p className="text-lg md:text-xl font-medium text-center md:text-left">
-                {description}
-              </p>
+              <p className="text-xl font-medium">{description}</p>
             </div>
             {component}
           </div>
         ))}
-
-        <div className="flex flex-col md:flex-row justify-between bg-accent py-16 md:py-20 px-8 md:px-10 relative rounded-2xl">
-          <div className="flex flex-col w-full md:w-2/5 gap-8">
-            <h2 className="text-2xl md:text-3xl font-semibold text-center md:text-left">
+        <div
+          className="flex justify-between bg-accent py-20 px-10 relative"
+          style={{
+            borderRadius: "52px",
+          }}
+        >
+          <div className="flex flex-col w-2/5 gap-8">
+            <h2 className="text-3xl font-semibold">
               Let&apos;s make things happen
             </h2>
-            <p className="text-lg md:text-xl tracking-wide text-center md:text-left">
+            <p className="text-xl tracking-wide">
               Contact us today to learn more about how our digital marketing
               services can help your business grow and succeed online.
             </p>
             <Link
               href="/request-a-quote"
-              className="py-4 px-6 md:py-5 md:px-8 text-white text-lg bg-secondary rounded-xl w-fit flex items-center justify-center hover:scale-105 transition-all"
+              className="py-5 px-8 text-white text-lg bg-secondary rounded-xl w-fit flex items-center justify-center hover:scale-105 transition-all"
             >
-              Get your free proposal
+              Get your free porposal
             </Link>
           </div>
           <Image
             src="/assets/emoji_big.svg"
             alt="Hero"
-            width={500}
-            height={500}
-            className="absolute right-0 -top-12 md:-top-8"
+            width={600}
+            height={600}
+            className="absolute right-0 -top-8"
           />
         </div>
-
         {sections.slice(1).map(({ title, description, component }) => (
-          <div className="flex flex-col gap-24 px-4 md:px-0" key={title}>
-            <div className="flex flex-col md:flex-row gap-10 w-full md:w-2/3">
-              <h2 className="px-4 py-2 text-3xl md:text-5xl bg-primary rounded-lg font-semibold text-center md:text-left">
+          <div className="flex flex-col gap-24" key={title}>
+            <div className="flex gap-10 w-2/3">
+              <h2 className="px-2 py-1 text-5xl bg-primary rounded-lg font-semibold flex items-center justify-center text-nowrap">
                 {title}
               </h2>
-              <p className="text-lg md:text-xl font-medium text-center md:text-left">
-                {description}
-              </p>
+              <p className="text-xl font-medium">{description}</p>
             </div>
             {component}
           </div>
